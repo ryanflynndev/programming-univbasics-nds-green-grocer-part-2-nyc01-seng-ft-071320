7fc912ccd2cb0fbe52a0e29ee5cb59fe58e3aa56
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-12 18:25:35 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -18,7 +18,7 @@ def apply_coupons(cart, coupons)
       else 
         cart_item_with_coupon = {
           :item => couponed_item_name.
-          :price => 
+          :price => coupons[:counter]
         }
       end
     end
