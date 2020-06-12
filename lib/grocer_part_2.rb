d8255658689f4e1eca34ba955aedefe7b11bb057
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-12 19:19:48 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -42,5 +42,5 @@ end
 def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
   couponed_cart = apply_coupons(consolidated_cart)
-  final_cart = apply_clearance()
+  final_cart = apply_clearance(couponed_cart)
 end
