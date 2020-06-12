069472652f9115ae8f1ec4f88c39fc98aee85e94
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-12 19:32:38 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -43,7 +43,7 @@ def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
   couponed_cart = apply_coupons(consolidated_cart, coupons)
   final_cart = apply_clearance(couponed_cart)
-  
+  total = 0
   final_cart.each do |element|
     total += element[:price] * element[:count]
   end 
