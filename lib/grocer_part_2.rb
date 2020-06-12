fc0fe38bdd743ab19217acbc9f544ea868b3d659
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-12 18:06:29 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -10,7 +10,7 @@ def apply_coupons(cart, coupons)
     cart_item = find_item_by_name_in_collection(element[:item], cart)
     coupon_item_name = "#{element[:item} W/COUPON"
     cart_item_with_coupon = find_item_by_name_in_collection(coupon_item_name, cart)
-    if cart
+    if cart_item
   end
   
 end
