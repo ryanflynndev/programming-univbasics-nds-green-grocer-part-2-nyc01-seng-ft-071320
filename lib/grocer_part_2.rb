8906a2e338985de01e2030b2e44709fb8c27fdb8
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-12 18:22:11 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -12,7 +12,7 @@ def apply_coupons(cart, coupons)
     cart_item_with_coupon = find_item_by_name_in_collection(coupon_item_name, cart)
     if cart_item && cart_item[:count] >= element[:num]
       if cart_item_with_coupon
-        cart_item_with_coupon[:count] += 
+        cart_item_with_coupon[:count] += element[:num]
       end
     end
   end
