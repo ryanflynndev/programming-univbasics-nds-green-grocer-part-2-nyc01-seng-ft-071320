29da53b683181e3627f14f4b23b399031c43cbbb
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-12 18:34:30 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -8,7 +8,7 @@ def apply_coupons(cart, coupons)
   
   coupons.each do |element|
     cart_item = find_item_by_name_in_collection(element[:item], cart)
-    couponed_item_name = "#{element[:item} W/COUPON"
+    couponed_item_name = "#{element[:item]} W/COUPON"
     cart_item_with_coupon = find_item_by_name_in_collection(coupon_item_name, cart)
     if cart_item && cart_item[:count] >= element[:num]
       if cart_item_with_coupon
